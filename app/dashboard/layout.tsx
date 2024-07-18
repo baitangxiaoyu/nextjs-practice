f3540c5a25d09fa8a6164e89b6efe91399d6b2d6
tangshuang <tangshuang@2dfire.com>
2024-07-18 18:12:54 +0800

    feat:Partial Prerendering

--- app/dashboard/layout.tsx
+++ app/dashboard/layout.tsx
@@ -1,6 +1,8 @@
 import React from 'react'
 import SideNav from '../ui/dashboard/sidenav'
 
+export const experimental_ppr = true;
+
 interface ILayoutProps extends React.PropsWithChildren { }
 function Layout(props: ILayoutProps) {
   return (
